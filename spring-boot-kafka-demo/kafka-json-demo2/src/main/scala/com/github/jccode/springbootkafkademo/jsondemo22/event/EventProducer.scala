65aaf4404b5b6beda8f0a28be8a04583e4809ebb
jcchen <junchangchen@gmail.com>
2018-03-07 00:42:45 +0800

    ..

--- spring-boot-kafka-demo/kafka-json-demo2/src/main/scala/com/github/jccode/springbootkafkademo/jsondemo22/event/EventProducer.scala
+++ spring-boot-kafka-demo/kafka-json-demo2/src/main/scala/com/github/jccode/springbootkafkademo/jsondemo22/event/EventProducer.scala
@@ -1,9 +1,31 @@
 package com.github.jccode.springbootkafkademo.jsondemo22.event
 
+import org.slf4j.{Logger, LoggerFactory}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
+import org.springframework.util.concurrent.ListenableFuture
+
+import scala.collection.mutable.ListBuffer
 
 @Component
 class EventProducer {
 
+  @Autowired
+  private val publisher: Publisher = null
+
+  private val log: Logger = LoggerFactory.getLogger(classOf[EventProducer])
+
+  def produceEvents1() = {
+    for (i <- 1 to 10) {
+      var tokens = ListBuffer[String]()
+      for (j <- 1 to i) {
+        tokens += j.toString
+      }
+      val tokenEvent: Event[ListBuffer[String]] = Event("tokens", tokens)
+      val value: ListenableFuture[SendResult[String, _]] = publisher.send(Constant.EventTopic, i.toString, tokenEvent)
+
+    }
+  }
 
 }
--- spring-boot-kafka-demo/kafka-json-demo2/src/main/scala/com/github/jccode/springbootkafkademo/jsondemo22/event/Publisher.scala
+++ spring-boot-kafka-demo/kafka-json-demo2/src/main/scala/com/github/jccode/springbootkafkademo/jsondemo22/event/Publisher.scala
@@ -2,7 +2,11 @@ package com.github.jccode.springbootkafkademo.jsondemo22.event
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.{KafkaHeaders, SendResult}
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Component
+import org.springframework.util.concurrent.ListenableFuture
 
 @Component
 class Publisher {
@@ -10,5 +14,16 @@ class Publisher {
   @Autowired
   private val template: KafkaTemplate[String, _] = null
 
+  def send(message: Message[_]): (ListenableFuture[SendResult[String, _]]) = template.send(message)
+
+  def send[T](topic: String, t: T): ListenableFuture[SendResult[String, _]] = {
+    val message: Message[T] = MessageBuilder.withPayload(t).setHeader(KafkaHeaders.TOPIC, topic).build()
+    send(message)
+  }
+
+  def send[T](topic: String, key: String, t: T): ListenableFuture[SendResult[String, _]] = {
+    val message: Message[T] = MessageBuilder.withPayload(t).setHeader(KafkaHeaders.TOPIC, topic).setHeader(KafkaHeaders.MESSAGE_KEY, key).build()
+    send(message)
+  }
 
 }
