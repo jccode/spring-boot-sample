4c978e4f1531884d093cac96ccd63178b9c9142e
jcchen <junchangchen@gmail.com>
2018-06-06 19:14:16 +0800

    update

--- spring-boot-security/rbac-guide/src/main/java/com/github/jccode/springsecuritydemo/rbacguide/Application.java
+++ spring-boot-security/rbac-guide/src/main/java/com/github/jccode/springsecuritydemo/rbacguide/Application.java
@@ -1,7 +1,9 @@
 package com.github.jccode.springsecuritydemo.rbacguide;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Application
@@ -13,6 +15,13 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("fuck");
+        };
     }
 
 }
---
+++ spring-boot-security/rbac-guide/src/main/resources/db/hsqldb/schema.sql
@@ -0,0 +1,29 @@
+create table user (
+  id int not null identity ,
+  username varchar(64) not null ,
+  password varchar(64) not null ,
+  salt varchar(128) not null ,
+  enabled boolean default true not null
+);
+
+create table role (
+  id int not null identity ,
+  name varchar(64) not null
+);
+
+create table permission (
+  id int not null identity ,
+  name varchar(64) not null
+);
+
+create table user_role (
+  id int not null identity ,
+  user_id int not null ,
+  role_id int not null
+);
+
+create table role_permission (
+  id int not null identity ,
+  role_id int not null ,
+  permission_id int not null
+);
