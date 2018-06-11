b1cd7c9fa33a2f821a7649221d3b9cb9b277b71b
jcchen <junchangchen@gmail.com>
2018-06-11 21:29:58 +0800

    oauth2 guide

---
+++ spring-boot-security/oauth2-guide/src/main/java/com/github/jccode/springsecuritydemo/oauthguide/Application.java
@@ -0,0 +1,18 @@
+package com.github.jccode.springsecuritydemo.oauthguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+
+@EnableResourceServer
+@EnableAuthorizationServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
