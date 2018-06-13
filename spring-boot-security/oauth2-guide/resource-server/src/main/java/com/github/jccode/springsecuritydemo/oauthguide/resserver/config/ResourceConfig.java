188d0a7a78a2c2fe2bc01394c0f03e9efb789a58
jcchen <junchangchen@gmail.com>
2018-06-14 01:57:22 +0800

    guide

--- spring-boot-security/oauth2-guide/resource-server/src/main/java/com/github/jccode/springsecuritydemo/oauthguide/resserver/config/ResourceConfig.java
+++ spring-boot-security/oauth2-guide/resource-server/src/main/java/com/github/jccode/springsecuritydemo/oauthguide/resserver/config/ResourceConfig.java
@@ -1,16 +1,32 @@
 package com.github.jccode.springsecuritydemo.oauthguide.resserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableResourceServer
 public class ResourceConfig extends ResourceServerConfigurerAdapter {
 
+    private static final String RESOURCE_ID = "home";
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        super.configure(resources);
+        resources.resourceId(RESOURCE_ID).tokenStore(new JdbcTokenStore(dataSource));
     }
+
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+//    }
+
 }
