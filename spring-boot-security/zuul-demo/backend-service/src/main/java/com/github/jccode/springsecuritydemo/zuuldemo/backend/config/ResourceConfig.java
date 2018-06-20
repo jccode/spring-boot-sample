5815c4f644ab01edf5019bb9ef966dd447e8fbbb
jcchen <junchangchen@gmail.com>
2018-06-20 18:22:01 +0800

    refactor

--- spring-boot-security/zuul-demo/backend-service/src/main/java/com/github/jccode/springsecuritydemo/zuuldemo/backend/config/ResourceConfig.java
+++ spring-boot-security/zuul-demo/backend-service/src/main/java/com/github/jccode/springsecuritydemo/zuuldemo/backend/config/ResourceConfig.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Import(SecurityCommonConfig.class)
 @Configuration
@@ -19,11 +17,11 @@ public class ResourceConfig extends ResourceServerConfigurerAdapter {
     private static final String RESOURCE_ID = "backend";
 
     @Autowired
-    private DataSource dataSource;
+    private TokenStore tokenStore;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId(RESOURCE_ID).tokenStore(new JdbcTokenStore(dataSource));
+        resources.resourceId(RESOURCE_ID).tokenStore(tokenStore);
     }
 
 //    @Override
--- spring-boot-security/zuul-demo/zuul-demo-common/src/main/java/com/github/jccode/springsecuritydemo/zuuldemo/common/SecurityCommonConfig.java
+++ spring-boot-security/zuul-demo/zuul-demo-common/src/main/java/com/github/jccode/springsecuritydemo/zuuldemo/common/SecurityCommonConfig.java
@@ -4,15 +4,26 @@ import com.allanditzel.springframework.security.web.csrf.CsrfTokenResponseHeader
 import com.github.jccode.springsecurity.common.authentication.RESTAuthenticationEntryPoint;
 import com.github.jccode.springsecurity.common.authentication.RESTAuthenticationFailureHandler;
 import com.github.jccode.springsecurity.common.authentication.RESTAuthenticationSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
+
+import javax.sql.DataSource;
 
 /**
  * WebSecurityConfig
@@ -25,6 +36,9 @@ import org.springframework.security.web.csrf.CsrfFilter;
 @EnableGlobalAuthentication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityCommonConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -46,4 +60,15 @@ public class SecurityCommonConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
         // @formatter:on
     }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
